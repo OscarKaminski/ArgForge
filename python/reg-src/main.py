c0d560d5198fd775e9885f69bf320329d0a09421
Oscar Kamiński <oscar.kaminski@oscarkaminski.com>
2025-04-20 23:33:50 +0100

    Update main.py

--- python/reg-src/main.py
+++ python/reg-src/main.py
@@ -1,8 +1,13 @@
 import os
 from openai import OpenAI
 from google import genai
+from brave import Brave
+from colorama import Fore, Style
+from colorama import init as colorama_init
 
-# Make sure all input values in the query() function are string values, although config_arg_num should be an integer inputed as a string.
+colorama_init()
+
+# Generate New Queries
 
 def gen_query(config_client, config_key, config_model, config_query, config_arg_num):
 
@@ -17,7 +22,7 @@ def gen_query(config_client, config_key, config_model, config_query, config_arg_
 
         print(response.output_text)
 
-    if config_client == "Google":
+    elif config_client == "Google":
 
         client = genai.Client(api_key = config_key)
 
@@ -28,5 +33,27 @@ def gen_query(config_client, config_key, config_model, config_query, config_arg_
 
         print(response.text)
 
-def search_query():
-    pass
+    else:
+        print(f"{Fore.RED}ERROR: Invalid/Unsupported Client.{Style.RESET_ALL}")
+
+# Search Generated Queries (WIP)
+
+#def search_query(config_client, config_key, config_query, config_arg_num, config_rslt_num):
+
+#    if config_client == "Brave":
+
+#       brave = Brave(api_key = config_key)
+
+#       try:
+#           search_results = brave.search(q = config_query, count = config_rslt_num)
+            
+#           for result in search_results.web_results:
+#               print(f"Title: {result.title}")
+#               print(f"URL: {result.link}")
+#               print(f"Description: {result.description}\n")
+                
+#       except Exception as e:
+#           print(f"Error: {str(e)}")
+
+#   else:
+#        print(f"{Fore.RED}ERROR: Invalid/Unsupported Client.{Style.RESET_ALL}")
